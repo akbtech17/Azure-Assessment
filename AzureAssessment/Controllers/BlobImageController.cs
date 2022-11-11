c963ce5e69156157c5ded004f65efd80861801f7
anshul-lti <AnshulKumar.Bansal@lntinfotech.com>
2022-11-11 13:56:11 +0530

    fixed the image upload

--- AzureAssessment/Controllers/BlobImageController.cs
+++ AzureAssessment/Controllers/BlobImageController.cs
@@ -2,24 +2,24 @@
 using Azure.Storage.Blobs.Models;
 using AzureAssessment.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
 using System.Net;
+using System.Reflection;
+using System.Reflection.Metadata;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace AzureAssessment.Controllers
 {
 	public class BlobImageController : Controller
 	{
-		[Obsolete]
-		private readonly IHostingEnvironment _environment;
 		private static string? _containerName;
 		private static string? _connectionString;
 		BlobServiceClient _serviceClient;
 		BlobContainerClient _containerClient;
 
 		[Obsolete]
-		public BlobImageController(IHostingEnvironment environment)
+		public BlobImageController()
 		{
-			_environment = environment;
 			var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false);
 			IConfiguration configuration = builder.Build();
 			_connectionString = configuration.GetValue<string>("ConnectionStrings:BlobConnectionString");
@@ -38,18 +38,13 @@ namespace AzureAssessment.Controllers
 		[HttpPost]
 		public IActionResult UploadImageToBlob(Image imageInfo) 
 		{
-			if (imageInfo !=null && imageInfo.MyImage != null) {
-				string fileName = Path.GetFileName(imageInfo.MyImage.FileName);			
-				string filePath = "C:\\Users\\10711704\\Downloads";
-				filePath = Path.Combine(filePath, fileName);	
-
-				/*IFormFile img = imageInfo.MyImage;
-				string? imgCaption = imageInfo.ImageCaption;*/
-
+			if (imageInfo != null && imageInfo.MyImage != null) 
+			{
+				IFormFile image = imageInfo.MyImage;
+				string? imgCaption = imageInfo.ImageCaption;
 				string? contentType = imageInfo.MyImage.ContentType;
-				BlobClient blobClient = _containerClient.GetBlobClient(fileName);
-				blobClient.Upload(filePath);
-				Console.WriteLine("New blob is created");
+
+				UploadImageBlob(image);
 			}
 			return View();
 		}
@@ -62,6 +57,16 @@ namespace AzureAssessment.Controllers
 				images.Add(new Image { ImageName = item.Name, ImageUrl = "https://storageaccount94111.blob.core.windows.net/images/"+item.Name });
 			}
 			return View(images);
+		}
+
+		public void UploadImageBlob(IFormFile image)
+		{
+			BlobClient blobClient = _containerClient.GetBlobClient(image.FileName);
+			using (var stream = image.OpenReadStream())
+			{
+				blobClient.Upload(stream);
+			}
+			Console.WriteLine($"{image.FileName} has been uploaded to {_containerName} conatainer of Blob Storage");
 		}
 	}
 }
