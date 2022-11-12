2d870d526fdabe1b85299d1951face4cdb6fd2d8
anshul bansal <akbt.tech17@gmail.com>
2022-11-12 12:08:53 +0530

    some css fixes

--- AzureAssessment/Models/Image.cs
+++ AzureAssessment/Models/Image.cs
@@ -4,13 +4,13 @@ namespace AzureAssessment.Models
 {
 	public class Image
 	{
-        [Required(ErrorMessage = "*caption is required")]
+        [Required(ErrorMessage = "* caption is required")]
         public string? ImageCaption { get; set; }
 
-        [Required(ErrorMessage = "*description is required")]
+        [Required(ErrorMessage = "* description is required")]
         public string? ImageDescription { get; set; }
 
-        [Required(ErrorMessage = "*select image")]
+        [Required(ErrorMessage = "* please select image")]
         public IFormFile? MyImage { get; set; }
 		public string? ImageName { get; set; }
 		public string? ImageUrl { get; set; }
