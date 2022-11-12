3cec9104a90b320be680ec66e46b5ea37b0e20de
anshul bansal <akbt.tech17@gmail.com>
2022-11-12 09:38:03 +0530

    fetching kv secrets

--- AzureAssessment/Controllers/KeyVaultController.cs
+++ AzureAssessment/Controllers/KeyVaultController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AzureAssessment.Controllers
 {
 	public class KeyVaultController : Controller
 	{
+		private static string? _keyVaultUrl;
+		private static SecretClient? _secretsClient;
+		//private static string? _sqlConnString;
+        public KeyVaultController(IConfiguration configuration) {
+			_keyVaultUrl = configuration["AzureKeyVaultUrl"];
+			_secretsClient = new SecretClient(new Uri(_keyVaultUrl), new DefaultAzureCredential());
+			var _sqlConnString = _secretsClient.GetSecret("AdminPassword");
+		}
 		public IActionResult Index()
 		{
 			return View();
