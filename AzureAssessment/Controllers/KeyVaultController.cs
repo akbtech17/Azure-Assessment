f1d24385e5eeabf7e1fe89aeb2fffcc6466c0529
anshul bansal <akbt.tech17@gmail.com>
2022-11-12 10:01:54 +0530

    getting access to kv using Application Object

--- AzureAssessment/Controllers/KeyVaultController.cs
+++ AzureAssessment/Controllers/KeyVaultController.cs
@@ -6,13 +6,24 @@ namespace AzureAssessment.Controllers
 {
 	public class KeyVaultController : Controller
 	{
-		private static string? _keyVaultUrl;
+		private static string? _tenantId;
+		private static string? _clientId;
+		private static string? _clientSecret;
+
+        private static string? _keyVaultUrl;
 		private static SecretClient? _secretsClient;
 		//private static string? _sqlConnString;
         public KeyVaultController(IConfiguration configuration) {
-			_keyVaultUrl = configuration["AzureKeyVaultUrl"];
-			_secretsClient = new SecretClient(new Uri(_keyVaultUrl), new DefaultAzureCredential());
-			var _sqlConnString = _secretsClient.GetSecret("AdminPassword");
+			_tenantId = configuration["TenantId"];
+			_clientId = configuration["ClientId"];
+			_clientSecret = configuration["ClientSecret"];
+
+			ClientSecretCredential clientCredential = new ClientSecretCredential(_tenantId, _clientId, _clientSecret);
+
+            _keyVaultUrl = configuration["AzureKeyVaultUrl"];
+            //_secretsClient = new SecretClient(new Uri(_keyVaultUrl), new DefaultAzureCredential());
+            _secretsClient = new SecretClient(new Uri(_keyVaultUrl), clientCredential);
+            var _sqlConnString = _secretsClient.GetSecret("AdminPassword");
 		}
 		public IActionResult Index()
 		{
