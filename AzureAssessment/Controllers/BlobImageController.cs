e8f34d3448a1c13fa0cf3d4a5fd082d58eb225c7
anshul bansal <akbt.tech17@gmail.com>
2022-11-14 12:11:35 +0530

    blob image is almost final

--- AzureAssessment/Controllers/BlobImageController.cs
+++ AzureAssessment/Controllers/BlobImageController.cs
@@ -3,11 +3,6 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using AzureAssessment.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting.Internal;
-using System.Net;
-using System.Reflection;
-using System.Reflection.Metadata;
-using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace AzureAssessment.Controllers
 {
@@ -15,17 +10,18 @@ namespace AzureAssessment.Controllers
 	{
 		private static string? _containerName;
 		private static string? _connectionString;
-		BlobServiceClient? _serviceClient;
+		private static string? _imageBaseUrl;
+        BlobServiceClient? _serviceClient;
 		BlobContainerClient? _containerClient;
 
-		public BlobImageController()
+
+		public BlobImageController(IConfiguration configuration)
 		{
-			try 
-			{
-                var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false);
-                IConfiguration configuration = builder.Build();
+			try
+			{ 
                 _connectionString = configuration.GetValue<string>("ConnectionStrings:BlobConnectionString");
                 _containerName = configuration.GetValue<string>("BlobContainerName");
+				_imageBaseUrl = configuration.GetValue<string>("BlobImagesBaseUrl");
 
                 _serviceClient = new BlobServiceClient(_connectionString);
                 _containerClient = _serviceClient.GetBlobContainerClient(_containerName);
@@ -59,9 +55,11 @@ namespace AzureAssessment.Controllers
 		public IActionResult ImageList() 
 		{
 			List<Image> images = new List<Image>();
+			if (_containerClient == null) return View(images);
+
             foreach (BlobItem item in _containerClient.GetBlobs())
 			{
-                images.Add(new Image { ImageName = item.Name, ImageUrl = "https://storageaccount94111.blob.core.windows.net/images/"+item.Name});
+                images.Add(new Image { ImageName = item.Name, ImageUrl = _imageBaseUrl+item.Name});
 			}
 			return View(images);
 		}
@@ -70,13 +68,13 @@ namespace AzureAssessment.Controllers
 		{
 			try 
 			{
-                BlobClient blobClient = _containerClient.GetBlobClient(image.FileName);
-                using (var stream = image.OpenReadStream()) blobClient.Upload(stream);
+                BlobClient? blobClient = _containerClient?.GetBlobClient(image.FileName);
+                using (var stream = image.OpenReadStream()) blobClient?.Upload(stream);
 
                 IDictionary<string, string> metadata = new Dictionary<string, string>();
                 if (caption != null) metadata.Add("Caption", caption);
                 if (descritpion != null) metadata.Add("Description", descritpion);
-                blobClient.SetMetadata(metadata);
+                blobClient?.SetMetadata(metadata);
                 Console.WriteLine($"{image.FileName} has been uploaded to {_containerName} conatainer of Blob Storage");
             }
 			catch (RequestFailedException ex) {
