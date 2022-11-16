a3301b0abba8f3e99912c312866e7a1464b1e670
anshul bansal <akbt.tech17@gmail.com>
2022-11-16 10:03:42 +0530

    some error validations

--- AzureAssessment/Models/KeyVault.cs
+++ AzureAssessment/Models/KeyVault.cs
@@ -1,7 +1,10 @@
-﻿namespace AzureAssessment.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AzureAssessment.Models
 {
     public class KeyVault
     {
+        [Required(ErrorMessage = "* key can't be empty")]
         public string? Key { get; set; }
         public string? Value { get; set; }
     }
--- AzureAssessment/Models/RecieveMessages.cs
+++ AzureAssessment/Models/RecieveMessages.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static Microsoft.Azure.Amqp.Serialization.SerializableType;
 
 namespace AzureAssessment.Models
 {
 	public class RecieveMessages
 	{
         [Required(ErrorMessage = "* message can't be empty")]
+        [Range(1, 10, ErrorMessage = "* enter values betweeen 1-10")]
         public int MessageCount { get; set; }
 		public List<string>? Messages { get; set; }
 	}
